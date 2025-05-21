632c2a6d7b408fe30ea35e1b4932778358438b1a
maldi2306 <aldi23ti@mahasiswa.pcr.ac.id>
2025-05-21 14:40:59 +0700

    commit ganti nama

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Maldi + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +24,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+       Hello Boss
       </p>
     </>
   )
